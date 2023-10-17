e645323b0a7d4ff7384317187e9bae2ea69cca66
kalpesh-11 <kalpeshbhagat1178@gmail.com>
2023-10-17 13:05:38 +0530

    Updated: about me

--- components/AboutDetails.tsx
+++ components/AboutDetails.tsx
@@ -4,24 +4,33 @@ export default function AboutDetails() {
   return (
     <div className="text-sm md:text-base">
       <p className="mb-5">
-        I'm a web enthusiast on a journey to become a seasoned frontend
-        developer. With over 3+ years of experience in web development, I've
-        specialized in crafting custom WordPress plugins that cater to unique
-        client needs. Now, I'm on the cusp of an exciting transition towards
-        becoming a full-stack developer.
+        I'm a web enthusiast who aspires to master
+        <span className="text-slate-200 font-medium">
+          {" "}
+          full-stack development
+        </span>
+        . With over 3+ years of experience in web development, I've specialized
+        in crafting custom WordPress plugins that cater to unique client needs.
+        Now, I'm taking the exciting step toward becoming a full-stack
+        developer.
       </p>
       <p className="mb-5">
-        I thrive on emerging technologies, opening doors to captivating web
-        experiences. I'm constantly expanding my toolkit to stay at the
+        I get excited about new technologies because they make web experiences
+        more interesting. I'm constantly expanding my toolkit to stay at the
         forefront of web development. My goal is to become a developer who
         shapes the digital landscape with innovative, user-centric solutions,
-        guided by the principle of Kaizen—embracing continuous improvement in
-        both my skills and creations.
+        guided by the principle of
+        <span className="text-slate-200 font-medium">
+          {" "}
+          Kaizen—embracing continuous improvement{" "}
+        </span>
+        in both my skills and creations.
       </p>
       <p>
-        Beyond coding, I'm an avid solo traveler. My journeys fuel creativity
-        and embody Kaizen—constant self-improvement. These adventures broaden my
-        perspective, enriching my experiences.
+        Beyond coding, I'm an also a
+        <span className="text-slate-200 font-medium"> solo traveler</span>. My
+        journeys fuel creativity, broaden my perspective, and enrich my
+        experiences. They encourage continuous self-improvement.
       </p>
     </div>
   );
