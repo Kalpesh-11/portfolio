ab2fec25320501f9bfb63668b178f0b0d8915fb0
kalpeshBhagat <kalpesh@caseproof.com>
2024-01-18 23:48:03 +0530

    Updated: experience details

--- components/ExperienceWrap.tsx
+++ components/ExperienceWrap.tsx
@@ -18,7 +18,7 @@ export default function ExperienceWrap() {
         />
       ))}
       <Link
-        href="/Kalpesh_bhagat.pdf"
+        href="/kalpesh_bhagat.pdf"
         target="_blank"
         className="hover-animation text-sm lg:text-base"
       >
